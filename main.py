31b638b48fb35d505daf66acb7af3c89f352a16b
Santiago Niño <a01665906@tec.mx>
2025-03-11 09:34:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import aiocron
+import requests
 
 app = FastAPI()
 
+origins = ["*"]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class FormData(BaseModel):
+    primero: str
+    segundo: str
+
+@aiocron.crontab("*/5 * * * *")
+async def self_ping():
+    response=requests.get("https://testfastapi-y81m.onrender.com")
+    print(response["message"])
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.post("/suma")
+def suma(item: Annotated[FormData, Form()]):
+    x = int(item.primero)
+    y = int(item.segundo)
+    resultado = x+y
+    return str(resultado)
